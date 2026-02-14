19025d99036303d83fa124893d614d4beec04d5d
tawfiqkhalilieh <taw.coding@gmail.com>
2026-02-14 17:45:56 +0200

    Finalize a minimalistic function

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-
-
 import run_stockfish
 
-print(run_stockfish.run_stockfish())
+
+out: tuple[int, ] = run_stockfish.run_stockfish("r1bqkbnr/ppp1pppp/2n5/3p4/3P4/5N2/PPP1PPPP/RNBQKB1R w KQkq - 2 3", 5, 5)
+
+assert type(out) == tuple
+assert len(out) == 5
+
+print(out)
+        
 
